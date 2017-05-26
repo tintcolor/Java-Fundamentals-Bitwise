b84029804acef008b89907ecc1d40661eaa5c5b7
ant <anthonyjones@zipcoder3.local>
2017-05-26 14:31:32 -0400

    Finished Lab

--- src/main/java/LightSwitcher.java
+++ src/main/java/LightSwitcher.java
@@ -23,7 +23,7 @@ public class LightSwitcher {
      * Remember to use bit notation (0bxxxxxxxx) and a bit operator.
      */
     public static int turnOnAllSwitches(int switches) {
-        return switches|0b11111111;
+        return switches | 0b11111111;
     }
 
     /**
@@ -32,57 +32,59 @@ public class LightSwitcher {
      * And a '1' in a position in 'switchesToTurnOff' means to turn that switch to off.
      */
     public static int turnOffSwitches(int switches, int switchesToTurnOff) {
-        return switches&~switchesToTurnOff;
+        return switches & ~switchesToTurnOff;
     }
-//
-//    /**
-//     * Take a given input of switches and turn them all off.
-//     * Remember to use bit notation and a bit operator.
-//     */
-//    public static int turnOffAllSwitches(int switches) {
-//
-//    }
-//
-//    /**
-//     * Take a given input of switches and the switches to flip, and return the new state of the switches.
-//     * NOTE: Wherever there is a '1' in switchesToFlip, flip the state of that switch in switches.
-//     * I.E switches = 1 0 1 and switchesToFlip = 1 1 0 should return 0 1 1.
-//     */
-//    public static int flipSwitches(int switches, int switchesToFlip) {
-//
-//    }
-//
-//    /**
-//     * Take a given input of switches and flip them all.
-//     * NOTE: An integer has more than 8 bits, so find a way to only return the rightmost 8 bits.
-//     */
-//    public static int flipAllSwitches(int switches) {
-//
-//    }
-//
-//    /**
-//     * Return the state of a switch in a given position.
-//     * Count switches from 0, and from right to left.
-//     * So, a byte reads 76543210
-//     */
-//    public static int getSwitchPositionAt(int switches, int position) {
-//
-//    }
-//
-//    /**
-//     * Move all the the bits to the right `count` places.
-//     */
-//    public static int moveRightBy(int switches, int count) {
-//
-//    }
-//
-//    /**
-//     * Move all the the bits to the left `count` places.
-//     * NOTE: An integer has more than 8 bits, so find a way to only return the rightmost 8 bits.
-//     */
-//    public static int moveLeftBy(int switches, int count){
-//
-//    }
+
+    /**
+     * Take a given input of switches and turn them all off.
+     * Remember to use bit notation and a bit operator.
+     */
+    public static int turnOffAllSwitches(int switches) {
+
+        return switches & 0b00000000;
+    }
+
+    /**
+     * Take a given input of switches and the switches to flip, and return the new state of the switches.
+     * NOTE: Wherever there is a '1' in switchesToFlip, flip the state of that switch in switches.
+     * I.E switches = 1 0 1 and switchesToFlip = 1 1 0 should return 0 1 1.
+     */
+    public static int flipSwitches(int switches, int switchesToFlip) {
+        return switches ^ switchesToFlip;
+    }
+
+    /**
+     * Take a given input of switches and flip them all.
+     * NOTE: An integer has more than 8 bits, so find a way to only return the rightmost 8 bits.
+     */
+    public static int flipAllSwitches(int switches) {
+
+        return switches ^ 0b11111111;
+    }
+
+    /**
+     * Return the state of a switch in a given position.
+     * Count switches from 0, and from right to left.
+     * So, a byte reads 76543210
+     */
+    public static int getSwitchPositionAt(int switches, int position) {
+        return switches >> position & 1;
+    }
+
+    /**
+     * Move all the the bits to the right `count` places.
+     */
+    public static int moveRightBy(int switches, int count) {
+        return switches >>> count;
+    }
+
+    /**
+     * Move all the the bits to the left `count` places.
+     * NOTE: An integer has more than 8 bits, so find a way to only return the rightmost 8 bits.
+     */
+    public static int moveLeftBy(int switches, int count) {
+        return switches << count & 0b11111111;
+    }
 
     /**
      * This is written for you to help with debugging.  If you call System.out.println(viewSwitches(switches)),
